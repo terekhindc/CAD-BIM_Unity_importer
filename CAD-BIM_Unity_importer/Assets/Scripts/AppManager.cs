bf0a9e49a7285e395da7f4e566a6e5e7338cd189
vrcorp <head@vrcorp.ru>
2019-04-30 15:40:19 +0700

    Создания экрана загрузки двух сцен и билд текущей версии

---
+++ CAD-BIM_Unity_importer/Assets/Scripts/AppManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+	}
+
+    public void StartLoadSCene (string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+}
---
+++ CAD-BIM_Unity_importer/Assets/Scripts/StartECS.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartECS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Spawner1")
+        {
+            BigCubeSpawner.InitalizeWithScene();
+        } else if (scene.name == "Spawner2")
+        {
+            CubeSpawner2.InitalizeWithScene();
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
