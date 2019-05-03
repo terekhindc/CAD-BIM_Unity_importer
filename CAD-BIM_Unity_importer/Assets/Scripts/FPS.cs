d25c74217e8e350dd44c0c15fcd975b4f89be39b
vrcorp <head@vrcorp.ru>
2019-05-03 20:56:29 +0700

    Перенесено на версию Unity 2019. Зафиксированы показатели среднего FPS: 100 тысяч объектов - 23 FPS; 75 тысяч объектов - 29 FPS; 50 тысяч объектов - 45 FPS; 25 тысяч объектов - 88 FPS

---
+++ CAD-BIM_Unity_importer/Assets/Scripts/FPS.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    [SerializeField] Text count;
+    [SerializeField] Text highestFPSLabel;
+    [SerializeField] Text averageFPSLabel;
+    [SerializeField] Text lowestFPSLabel;
+
+    int fps;
+    public int frameRange = 60;
+    int AverageFPS;
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+
+    static string[] stringsFrom00To99 = {
+        "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+        "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+        "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+        "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+        "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+        "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+        "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+        "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+        "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
+    };
+
+    void InitializeBuffer()
+    {
+        if (frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    void UpdateBuffer()
+    {
+        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange)
+        {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    void CalculateFPS()
+    {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++)
+        {
+            int fps = fpsBuffer[i];
+            sum += fps;
+            if (fps > highest)
+            {
+                highest = fps;
+            }
+            if (fps < lowest)
+            {
+                lowest = fps;
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+
+        fps = (int)(1f / Time.unscaledDeltaTime);        
+
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+        //count.text = "Count " + Samples.HelloCube_06.HelloSpawnerSystem.count;
+        highestFPSLabel.text = "Highest FPS " +
+            stringsFrom00To99[Mathf.Clamp(HighestFPS, 0, 99)];
+        averageFPSLabel.text = "Average FPS" +
+            stringsFrom00To99[Mathf.Clamp(AverageFPS, 0, 99)];
+        lowestFPSLabel.text = "Lowest FPS" +
+            stringsFrom00To99[Mathf.Clamp(LowestFPS, 0, 99)];
+    }
+}
